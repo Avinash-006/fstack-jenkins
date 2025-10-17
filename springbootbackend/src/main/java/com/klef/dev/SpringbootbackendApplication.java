665f8bbc8400eb59de84f24763bcc7210b09b130
Avinash <avinashdola57@gmail.com>
2025-10-18 01:40:26 +0530

    v1.2

--- springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
+++ springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
@@ -1,10 +1,10 @@
 package com.klef.dev;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootbackendApplication 
+public class SpringbootbackendApplication extends SpringBootServletInitializer
 {
 	public static void main(String[] args) 
 	{
